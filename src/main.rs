f1d887be0251a51e657d5c9129fef12725bcf1d5
EduartePaiva <eduarte2018.po@gmail.com>
2023-06-01 12:00:37 -0300

    mais rust
    terminado guessing_game cap2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Adivinhe o número!");
-    println!("Por favor insira seu palpite.");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Falhou ao ler a linha");
+    println!("O número secreto é: {secret_number}");
 
-    println!("Você palpitou: {guess}");
+    loop {
+        println!("Por favor insira seu palpite:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Falhou ao ler a linha");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Você palpitou: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
